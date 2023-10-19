1115989adbad64793d7fb2972bd42308dedac71b
Diegoeosuna <diegoemilioosuna@hotmail.com>
2023-10-18 21:39:14 -0600

    Corrección de findByID por findById

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -3,30 +3,30 @@ const asyncHandler = require('express-async-handler')
 const User = require('../models/usersModel')
 
 const protect = asyncHandler(async (req, res, next) => {
-    let token
+    let token 
     if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
-        try {
+        try{
             //obtenemos el token
             token = req.headers.authorization.split(' ')[1]
 
-            //verificar el token
+            //Verificar el token
             const decoded = jwt.verify(token, process.env.JWT_SECRET)
 
-            //obtener los datos del usuario del token
+            //Obtener los datos del ususario del token
             req.user = await User.findById(decoded.id).select('-password')
 
             next()
 
-        } catch (error) {
+        } catch (error){
             console.log(error)
             res.status(401)
-            throw new Error('Acceso no autorizado')
+            throw new Error('Acceso no autorizado, aqui')
         }
-    }
+    } 
 
-    if (!token) {
+    if(!token){
         res.status(401)
-        throw new Error('Acceso no autorizado, No se proporcionó el Token')
+        throw new Error ('Acceso no autorizado, no se proporcionó el token')
     }
 
 })
