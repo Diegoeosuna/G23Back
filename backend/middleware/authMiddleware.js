f9228e4ab276025e92b7167bdc38fe66cc58e1d1
Diegoeosuna <diegoemilioosuna@hotmail.com>
2023-10-18 21:25:20 -0600

    Copia de Auth

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -2,30 +2,31 @@ const jwt = require('jsonwebtoken')
 const asyncHandler = require('express-async-handler')
 const User = require('../models/usersModel')
 
-const protect = asyncHandler (async (req, res, next) => {
-    let token 
-    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')){
-        try{
+const protect = asyncHandler(async (req, res, next) => {
+    let token
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+        try {
             //obtenemos el token
             token = req.headers.authorization.split(' ')[1]
 
-            //Verificar el token
+            //verificar el token
             const decoded = jwt.verify(token, process.env.JWT_SECRET)
 
-            //Obtener los datos del ususario del token
-            req.user = await User.findByID(decoded.id).select('-password')
+            //obtener los datos del usuario del token
+            req.user = await User.findById(decoded.id).select('-password')
 
             next()
 
-        } catch (error){
+        } catch (error) {
             console.log(error)
             res.status(401)
-            throw new Error('Acceso no autorizado, aqui')
+            throw new Error('Acceso no autorizado')
         }
-    } 
-    if(!token){
+    }
+
+    if (!token) {
         res.status(401)
-        throw new Error ('Acceso no autorizado, no se proporcionó el token')
+        throw new Error('Acceso no autorizado, No se proporcionó el Token')
     }
 
 })
